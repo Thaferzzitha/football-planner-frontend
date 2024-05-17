02e5918317ff95ec1272ebb9d968beb26729648a
Thaferzzitha <thaferzzitha1@gmail.com>
2024-05-17 16:51:28 -0500

    first commit

---
+++ src/sections/auth/login/login-view.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { alpha, useTheme } from '@mui/material/styles';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { useRouter } from 'src/routes/hooks';
+
+import { bgGradient } from 'src/theme/css';
+
+import Iconify from 'src/components/iconify';
+
+// ----------------------------------------------------------------------
+
+export default function LoginView() {
+  const theme = useTheme();
+
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClick = () => {
+    const formData = {
+      email: document.getElementsByName('email')[0].value,
+      password: document.getElementsByName('password')[0].value,
+    };
+    console.log('Datos del formulario de iniciar sesión:', formData);
+    router.push('/dashboard');
+  };
+
+  const renderForm = (
+    <>
+      <Stack spacing={3}>
+        <TextField name="email" label="Correo electrónico" />
+
+        <TextField
+          name="password"
+          label="Contraseña"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+
+      <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{ my: 3 }}>
+        <Link variant="subtitle2" underline="hover">
+          ¿Olvidaste tu contraseña?
+        </Link>
+      </Stack>
+
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        color="info"
+        onClick={handleClick}
+      >
+        Iniciar Sesion
+      </LoadingButton>
+    </>
+  );
+
+  return (
+    <Box
+      sx={{
+        ...bgGradient({
+          color: alpha(theme.palette.background.default, 0.9),
+          imgUrl: '/assets/background/overlay_4.jpg',
+        }),
+        height: 1,
+      }}
+    >
+
+      <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
+        <Card
+          sx={{
+            p: 5,
+            width: 1,
+            maxWidth: 420,
+          }}
+        >
+          <Typography variant="h4">Iniciar Sesión</Typography>
+
+          <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
+            ¿Aún no tienes una cuenta?
+            <Link variant="subtitle2" sx={{ ml: 0.5 }} href="/register">
+              Registrarse
+            </Link>
+          </Typography>
+
+          {renderForm}
+        </Card>
+      </Stack>
+    </Box>
+  );
+}
---
+++ src/sections/auth/register/register-view.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { alpha, useTheme } from '@mui/material/styles';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { useRouter } from 'src/routes/hooks';
+
+import { bgGradient } from 'src/theme/css';
+
+import Iconify from 'src/components/iconify';
+
+// ----------------------------------------------------------------------
+
+export default function RegisterView() {
+  const theme = useTheme();
+
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClick = () => {
+    const formData = {
+      firstName: document.getElementsByName('firstName')[0].value,
+      lastName: document.getElementsByName('lastName')[0].value,
+      email: document.getElementsByName('email')[0].value,
+      password: document.getElementsByName('password')[0].value,
+    };
+    console.log('Datos del formulario de registro:', formData);
+    router.push('/dashboard');
+  };
+
+  const renderForm = (
+    <>
+       <Stack spacing={3} sx={{ mb: 5 }}>
+        <TextField name="firstName" label="Nombre" />
+        <TextField name="lastName" label="Apellido" />
+        <TextField name="email" label="Correo electrónico" />
+
+        <TextField
+          name="password"
+          label="Contraseña"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        color="info"
+        onClick={handleClick}
+      >
+        Registrarse
+      </LoadingButton>
+    </>
+  );
+
+  return (
+    <Box
+      sx={{
+        ...bgGradient({
+          color: alpha(theme.palette.background.default, 0.9),
+          imgUrl: '/assets/background/overlay_4.jpg',
+        }),
+        height: 1,
+      }}
+    >
+      <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
+        <Card
+          sx={{
+            p: 5,
+            width: 1,
+            maxWidth: 450,
+          }}
+        >
+          <Typography variant="h4">Registrarse</Typography>
+
+          <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
+            ¿Ya tienes una cuenta?
+            <Link variant="subtitle2" sx={{ ml: 0.5 }} href="/login">
+              Inicia sesión
+            </Link>
+          </Typography>
+
+          {renderForm}
+        </Card>
+      </Stack>
+    </Box>
+  );
+}
